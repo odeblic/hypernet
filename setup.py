9c5ce661175cba93f198f88a41b52fa501dfd1e9
defmax <defmax@laposte.net>
2019-05-20 11:06:34 +0800

    Get the dependencies from requirements.txt

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ import setuptools
 with open("README.md", "r") as f:
     long_description = f.read()
 
+with open("requirements.txt", "r") as f:
+    install_requires = list()
+    for requirement in f.readlines():
+        requirement = requirement.strip().split('#')[0].strip()
+        if len(requirement) > 0:
+            install_requires.append(requirement)
+
 setuptools.setup(
     name = "hypernet",
     version = "0.1",
@@ -17,11 +24,7 @@ setuptools.setup(
     python_requires = '>=3.6, <4',
     packages = setuptools.find_packages(),
     py_modules = ['hypernet'],
-    install_requires = [
-        'bidict',
-        'nltk',
-        'pyyaml',
-        ],
+    install_requires = install_requires,
     classifiers = [
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
