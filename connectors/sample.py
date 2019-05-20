8bd5caef6537bf7e3e9d3af1989aef9b0f1f639a
defmax <defmax@laposte.net>
2019-05-20 23:19:38 +0800

    Enhance the sample connector

--- connectors/sample.py
+++ connectors/sample.py
@@ -1,23 +1,39 @@
 from core.channel import Channel
 from core.connector import Connector
+from core.message import Message
+import time
 
 
 class Sample(Connector):
     """ A sample to show how to implement a plugged-in connector """
     def __init__(self):
-        super().__init__('sample', 7, 512, {'bot_id':111, 'net_id':999})
+        super().__init__('sample', 7, 512, {'Globot':0, 'Jack Eleven':11, 'Anna Five':5})
 
     def _on_schedule(self):
-        # example of incoming message
-        sender = 111
-        receiver = 999
-        conversation = None
-        channel = Channel(sender, receiver, conversation)
-        print('incoming message "{}" from {} to {} within {}'.format(message, sender, receiver, conversation))
-        self._push_received_message('Hello world', channel)
+        time.sleep(1)
+
+        # example of incoming message for the bot in private
+        message = Message.build('Hi @globot please ask for service #sample')
+        channel = Channel(11, 0, None)
+        print('connector:\tincoming message "{}" {}'.format(message, channel))
+        self._push_received_message(message, channel)
+
+        # example of incoming message for the bot in chatroom
+        message = Message.build('Hi @globot please ask for service #sample')
+        channel = Channel(60, 0, 777)
+        print('connector:\tincoming message "{}" {}'.format(message, channel))
+        self._push_received_message(message, channel)
+
+        # example of incoming message in chatroom for another agent
+        message = Message.build('Hi there, nothing new today...')
+        channel = Channel(11, 60, 777)
+        print('connector:\tincoming message "{}" {}'.format(message, channel))
+        self._push_received_message(message, channel)
 
         # example of outgoing message
-        (message, channel) = self._pop_message_to_send()
-        (sender, receiver, conversation) = channel
-        print('outgoing message "{}" from {} to {} within {}'.format(message, sender, receiver, conversation))
+        while True:
+            ret = self._pop_message_to_send()
+            if ret is None: break
+            (message, channel) = ret
+            print('connector:\toutgoing message "{}" {}'.format(message, channel))
 
