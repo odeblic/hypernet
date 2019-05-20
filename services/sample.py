b995350cddecf1b97960b07da05934fd066ff4f2
defmax <defmax@laposte.net>
2019-05-20 18:09:12 +0800

    Implement the callback in the sample service

--- services/sample.py
+++ services/sample.py
@@ -6,8 +6,12 @@ class Sample(Service):
     def __init__(self):
         super().__init__('sample', 8, None)
 
+    def _on_schedule(self):
+        pass
+
     def on_message(self, message, channel):
         sender = channel.get_sender()
+        receiver = channel.get_receiver()
         conversation = channel.get_conversation()
-        print('got message "{}" from {}@{}'.format(message, sender, conversation))
+        print('incoming message "{}" from {} to {} within {}'.format(message, sender, receiver, conversation))
 
