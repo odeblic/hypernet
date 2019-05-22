7d98bc605c9e6fba3a22c58fcd92b55e13150f93
defmax <defmax@laposte.net>
2019-05-22 19:48:37 +0800

    Make internal representation of messages more robust

--- core/message.py
+++ core/message.py
@@ -1,25 +1,36 @@
+import html
 import re
 
 
 class Message(object):
     """ Internal representation of a message within the framework """
+
+    PATTERN_MENTION = re.compile("^@[A-Z_a-z0-9]+$")
+    PATTERN_HASHTAG = re.compile("^#[A-Z_a-z0-9]+$")
+    PATTERN_NUMBER = re.compile("^[0-9]+$")
+    PATTERN_WORD = re.compile("^[A-Z_a-z0-9]+$")
+
     def __init__(self):
         self.__elements = list()
 
     @classmethod
     def build(cls, text):
         message = cls()
-        token_list = text.split()
-        for token in token_list:
-            if re.compile("^@[A-Za-z0-9]+$").match(token):
-                message.add_element(cls.Mention(token))
-            elif re.compile("^#[A-Za-z0-9]+$").match(token):
-                message.add_element(cls.Hashtag(token))
+        for token in text.split():
+            if Message.PATTERN_MENTION.match(token):
+                message._add_element(cls.Mention(token[1:]))
+            elif Message.PATTERN_HASHTAG.match(token):
+                message._add_element(cls.Hashtag(token[1:]))
+            elif Message.PATTERN_NUMBER.match(token):
+                message._add_element(cls.Number(token))
+            elif Message.PATTERN_WORD.match(token):
+                message._add_element(cls.Word(token))
             else:
-                message.add_element(cls.Word(token))
+                print('\033[31m{}\033[0m should maybe escaped as \033[32m{}\033[0m?'.format(token, html.escape(token)))
+                raise ValueError('{} is not a valid element value'.format(token))
         return message
 
-    def add_element(self, element):
+    def _add_element(self, element):
         self.__elements.append(element)
 
     def find_elements(self, element_type=object):
@@ -38,17 +49,22 @@ class Message(object):
     class Mention(str):
        """ A user mentioned such @user """
        def __new__(cls, *args, **kw):
-           return str.__new__(cls, *args, **kw)
+            return str.__new__(cls, *args, **kw)
 
     class Hashtag(str):
        """ A topic mentioned such #topic """
        def __new__(cls, *args, **kw):
-           return str.__new__(cls, *args, **kw)
+            return str.__new__(cls, *args, **kw)
 
     class Word(str):
-       """ Some plain text """
+       """ Some plain text such blablabla """
        def __new__(cls, *args, **kw):
-           return str.__new__(cls, *args, **kw)
+            return str.__new__(cls, *args, **kw)
+
+    class Number(int):
+       """ Some number such 409812 """
+       def __new__(cls, *args, **kw):
+            return int.__new__(cls, *args, **kw)
 
     class Form(object):
         def __init__(self):
@@ -65,10 +81,12 @@ class Message(object):
             if isinstance(element, self.Word):
                 tokens.append('\033[35m{}\033[0m'.format(element))
             elif isinstance(element, self.Mention):
-                tokens.append('\033[36m{}\033[0m'.format(element))
+                tokens.append('\033[36m@{}\033[0m'.format(element))
             elif isinstance(element, self.Hashtag):
-                tokens.append('\033[34m{}\033[0m'.format(element))
+                tokens.append('\033[34m#{}\033[0m'.format(element))
+            elif isinstance(element, self.Number):
+                tokens.append('\033[33m{}\033[0m'.format(element))
             else:
-                raise Exception('Invalid type for element')
+                raise TypeError('{} is not a valid element type'.format(type(element)))
         return ' '.join(tokens)
 
