56f7bfe0b15e0c53d4d51ddf8225614bbbab81b6
Olivier <olivier@hackathon.com>
2019-05-24 14:37:17 +0800

    Change the mention of receiver in Echo

--- services/echo.py
+++ services/echo.py
@@ -11,6 +11,7 @@ class Echo(Service):
         while len(self._incoming_messages) > 0:
             (message, channel) = self._incoming_messages.pop()
             channel = self._bot.reply(channel)
+            message.set_first_mention(channel.get_receiver())
             self._outgoing_messages.insert(0, (message, channel))
             logging.debug('service \033[32mecho\033[0m replied a message')
 
