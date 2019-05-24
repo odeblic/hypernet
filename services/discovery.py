7fdb2f4b90b8bb1b75848ca63d8b55fb839b926b
Olivier <olivier@hackathon.com>
2019-05-24 14:38:11 +0800

    Implenet the basic discovery

--- services/discovery.py
+++ services/discovery.py
@@ -7,20 +7,46 @@ import logging
 
 class Discovery(Service):
     """ Measure the latency to remote bots and print a report on demand """
-    INTERVAL = datetime.timedelta(seconds=3)
+    INTERVAL = datetime.timedelta(seconds=5)
 
     def __init__(self):
         super().__init__('discovery', 1, None)
-        self.__bots = dict({'bot89':1000000})
+        self.__bots = dict({'bot89':None})
         self.__pending = dict()
         self.__before = datetime.datetime.now()
+        self.__active = False
 
     def on_schedule(self):
-        return  # pattern req/resp or daemon?
+        # use a pattern req/resp or daemon?
         while len(self._incoming_messages) > 0:
             (message, channel) = self._incoming_messages.pop()
+            now = datetime.datetime.now()
 
-            # measure latency for responses if any
+            if 'start' in message.find_elements(message.__class__.Word):
+                logging.debug('service \033[32mdiscovery\033[0m is now active')
+                self.__active = True
+                for name in self.__bots.keys():
+                    self.__pending[name] = now
+                    message = Message.build('@bot89 #discovery 1')
+                    channel = Channel('bot73', 'bot89', 'cool', 'symphony')
+                    self._outgoing_messages.insert(0, (message, channel))
+                    logging.debug('service \033[32mdiscovery\033[0m sent a request')
+                self.__before = now
+            elif 'stop' in message.find_elements(message.__class__.Word):
+                logging.debug('service \033[32mdiscovery\033[0m is now inactive')
+                self.__active = False
+            elif 'status' in message.find_elements(message.__class__.Word):
+                logging.debug('service \033[32mdiscovery\033[0m makes a report')
+                bots = list()
+                for (name, latency) in self.__bots.items():
+                    bots.append('{} {}'.format(name, latency))
+                message = Message.build(' '.join(bots))
+                channel = self._bot.reply(channel)
+                self._outgoing_messages.insert(0, (message, channel))
+            #elif self.__active:
+            #    logging.debug('service \033[32mdiscovery\033[0m has been triggered')
+            #else:
+
             bot_name = channel.get_sender()
             if channel.get_sender() in self.__pending:
                 logging.debug('service \033[32mdiscovery\033[0m received a response')
@@ -28,26 +54,28 @@ class Discovery(Service):
                 response_time = datetime.datetime.now()
                 latency = response_time - request_time
                 self.__bots[bot_name] = latency.seconds
-            else:
-                logging.error('service \033[32mdiscovery\033[0m received a response by mistake')
+                
+                channel = self._bot.reply(channel)
+                message.set_first_mention(channel.get_receiver())
+                message.increment_numbers()
+                self._outgoing_messages.insert(0, (message, channel))
+            #else:
+            #    logging.error('service \033[32mdiscovery\033[0m received a response by mistake')
+
+        return
 
             # respond to requests if any
-            bots = list()
-            for (name, latency) in self.__bots.items():
-                bots.append('{} {}'.format(name, latency))
-            message = Message.build(' '.join(bots))
-            channel = self._bot.reply(channel)
-            self._outgoing_messages.insert(0, (message, channel))
-            logging.debug('service \033[32mdiscovery\033[0m has been requested')
 
         # periodically ping other bots
+        #if self.__active:
+        #self.__active = False
         now = datetime.datetime.now()
         if now - self.__before > self.INTERVAL:
             for name in self.__bots.keys():
                 self.__pending[name] = now
-                message = Message.build('@bot89 #echo #discovery')
+                message = Message.build('@bot89 #discovery 1')
                 channel = Channel('bot73', 'bot89', 'cool', 'symphony')
                 self._outgoing_messages.insert(0, (message, channel))
-                logging.debug('service \033[32mdiscovery\033[0m is searching...')
+                logging.debug('service \033[32mdiscovery\033[0m sent a request')
             self.__before = now
 
